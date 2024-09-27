781e975a01b07fd6a5125df7b41a25291be71eaa
zhiwarr <zhiwar04@gmail.com>
2024-09-27 13:57:06 +0300

    Route Parameters

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return "this is about page";
 });
+Route::get('/user/{id}',function($id){
+    return "this is user page with id: $id";
+});
