48dc68af6d16120f9df59d8d2b00b9d09e0da27a
zhiwarr <zhiwar04@gmail.com>
2024-09-27 14:09:37 +0300

    Route Groups

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,11 @@ Route::get('/about', function () {
 Route::get('/user/{id}/{slug}',function($id,$slug){
     return "this is user page with id: $id and slug: $slug";
 })->name('user.profile');
+Route::group(['prefix'=>'blog','as'=>'blog.'],function(){
+    Route::get('/create',function(){
+        return "this is create blog page";
+    });
+    Route::get('/edit/{id}',function($id){
+        return "this is edit blog page with id: $id";
+    });
+});
