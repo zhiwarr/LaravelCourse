540b6fd0caf076a453e7a43df8eedee34c1d62a3
zhiwarr <zhiwar04@gmail.com>
2024-09-27 19:22:38 +0300

    extend and include directives

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+</head>
+
+<body>
+
+    <a href="{{ route('about') }}">about</a>
+    <a href="{{ route('user.profile',['id'=>1,'slug'=>'zhiwar-osman']) }}">zhiwar</a>
+    @yield('content')
+    @include('layouts.footer')
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-    <a href="{{ route('about') }}">about</a>
-    <a href="{{ route('user.profile',['id'=>1,'slug'=>'zhiwar-osman']) }}">zhiwar</a>
-</body>
-
-</html>
+@extends('layouts.app')
+@section('title','welcome page')
+@section('content')
+<h1>Welcome section</h1>
+@endsection
