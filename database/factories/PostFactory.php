eafe526156f0a15dc22a2f7c462e0b4271afd88d
zhiwarr <zhiwar04@gmail.com>
2024-09-27 20:58:03 +0300

    factory and seeders

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(5),
+            'body' => $this->faker->paragraph(5),
+            'view_count'=>$this->faker->numberBetween(1,100)
+            // 'user_id' => User::factory(),
+        ];
+    }
+}
